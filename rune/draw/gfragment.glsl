e1091c4e2f03e52f433efa3c84e3f63424a260d5
Jay McCarthy <jay.mccarthy@gmail.com>
2013-06-20 13:05:19 -0600

    note

--- rune/draw/gfragment.glsl
+++ rune/draw/gfragment.glsl
@@ -10,6 +10,9 @@ out vec4 out_Color;
 void main ( void )
 {
   vec4 TexColor = texture2D(@|FontTex|, @|TexCoord|);
+
+  // 40ms. I doubt you could do faster, because when i remove the if
+  // and make it constant, it runs at the same speed.
   if (TexColor.r == 1.0) {
     out_Color = @|FColor|;
   } else {
