314070286bf2929dfac7fd55a9208a01c4936f49
Jay McCarthy <jay.mccarthy@gmail.com>
2014-01-25 11:49:09 -0700

    protect

--- rune2/socket.rkt
+++ rune2/socket.rkt
@@ -18,10 +18,6 @@
 (define-gtk gtk_socket_get_id (_fun _GtkWidget -> _int))
 
 (define-gtk gtk_widget_size_allocate (_fun _GtkWidget _GtkAllocation-pointer -> _void))
-(define-gdk gdk_get_default_root_window (_fun -> _GdkWindow))
-(define-gdk gdk_window_get_parent (_fun _GdkWindow -> _GdkWindow))
-(define-gdk gdk_window_foreign_new (_fun _int -> _GdkWindow))
-(define-gdk gdk_window_reparent (_fun _GdkWindow _GdkWindow _int _int -> _void))
 
 (define socket%
   (class* object% ()
@@ -32,44 +28,25 @@
 
     (gtk_container_add (send parent get-client-handle) gtk)
 
+    (define filled? #f)
+    (define (protect)
+      (when filled?
+        (error 'socket% "Can only be filled once."))
+      (set! filled? #t))
+
     (define/public (get-id)
+      (protect)
       (gtk_socket_get_id gtk))
 
-    (define embedded-id #f)
-    (define (reparent-old)
-      (when embedded-id
-        (printf "reparent ~a to ~a\n" embedded-id 'root)
-        (define fw (gdk_window_foreign_new embedded-id))
-        (gdk_window_reparent fw (gdk_get_default_root_window) 0 0)
-        (set! embedded-id #f)))
-    (define (new-embedding id)
-      (set! embedded-id id)
-      (set-last-size))
-
     (define/public (add-id id)
-      (reparent-old)
-      (gtk_socket_add_id gtk id)
-      (new-embedding id))
+      (protect)
+      (gtk_socket_add_id gtk id))
 
     (define/public (steal-id id)
-      (reparent-old)
-      (gtk_socket_steal gtk id)
-      (new-embedding id))
-
-    (define last-w #f)
-    (define last-h #f)
-
-    (define (set-last-size)
-      (when (and last-w last-h)
-        (really-set-size 0 0)
-        (really-set-size last-w last-h)))
+      (protect)
+      (gtk_socket_steal gtk id))
 
     (define/public (set-size w h)
-      (set! last-w w)
-      (set! last-h h)
-      (really-set-size w h))
-
-    (define (really-set-size w h)
       (define child-gtk gtk)
       (define x 0)
       (define y 0)
