cef7cc45d13a23e176feb0ce4d1b4dd1bffe5d38
Jay McCarthy <jay.mccarthy@gmail.com>
2015-04-30 20:12:44 -0400

    deleting old stuff

--- rune/lib/context.rkt
+++
@@ -1,5 +0,0 @@
-#lang racket/base
-(require racket/contract)
-(define context/c
-  (-> (-> any) any))
-(provide context/c)
--- rune/lib/timing.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket/base
-
-(define-syntax-rule (time-it e ...)
-  (let* ([before (current-inexact-milliseconds)]
-         [v (let () e ...)]
-         [after (current-inexact-milliseconds)])
-    (values (- after before) v)))
-
-(provide time-it)
