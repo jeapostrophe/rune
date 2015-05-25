10596948a71b9bf94442eca2c40cbda32a74933f
Jay McCarthy <jay.mccarthy@gmail.com>
2015-05-25 11:06:32 -0400

    json

--- runeW/index.js
+++ runeW/index.js
@@ -1,15 +1,15 @@
 window.onload = function() {
     var exampleSocket = new WebSocket("ws://localhost:7333/rune");
     window.onkeydown = function (e) {
-        exampleSocket.send("(key! " + e.keyIdentifier +
-                           " " + e.altKey +
-                           " " + e.ctrlKey +
-                           " " + e.metaKey +
-                           " " + e.shiftKey + ")");
+        exampleSocket.send(
+            JSON.stringify(
+                ["key!", e.keyIdentifier,
+                 e.altKey, e.ctrlKey, e.metaKey,
+                 e.shiftKey]));
         e.preventDefault();
     };
     exampleSocket.onopen = function (event) {
-        exampleSocket.send("(open!)");
+        exampleSocket.send(JSON.stringify(["open!"]));
     };
     exampleSocket.onmessage = function (event) {
         eval(event.data);
