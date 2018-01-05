a96d6f3891fe15d5093079474c216529241a3271
Jay McCarthy <jay.mccarthy@gmail.com>
2018-01-05 15:52:38 -0500

    rearranging

---
+++ rune/pager.rkt
@@ -0,0 +1,92 @@
+#lang racket/base
+(require racket/match
+         racket/set
+         racket/port
+         racket/format
+         data/gvector
+         lux
+         raart)
+
+(define (with-input-source src f)
+  ((match src
+     ['stdin call-with-closing-stdin]
+     [_ (λ (f) (call-with-input-file src (λ (ip) (f src ip))))])
+   f))
+(define (call-with-closing-stdin f)
+  (define cip (current-input-port))
+  (dynamic-wind void (λ () (f "stdin" cip)) (λ () (close-input-port cip))))
+(define (clamp m x M)
+  (min (max m x) M))
+
+(struct pager (src ip rows cols lines max-col row col)
+  #:methods gen:word
+  [(define (word-fps w) 0.0)
+   (define (word-label w ft)
+     (~a "rage: " (pager-src w)))
+   (define (word-evt w)
+     (define ip (pager-ip w))
+     (if (port-closed? ip)
+       never-evt
+       (handle-evt (read-line-evt ip 'linefeed)
+                   (λ (l)
+                     (when (eof-object? l)
+                       (close-input-port ip))
+                     l))))
+   (define (word-event w e)
+     (match e
+       [(== (key #\D (set 'control)))
+        #f]
+       [(? key?)
+        (define-values (dr dc)
+          (match (key-value e)
+            ['up    (values -1  0)]
+            ['down  (values +1  0)]
+            ['left  (values  0 -1)]
+            ['right (values  0 +1)]
+            [_      (values  0  0)]))
+        (struct-copy pager w
+                     [row (clamp 0 (+ (pager-row w) dr)
+                                 (gvector-count (pager-lines w)))]
+                     [col (clamp 0 (+ (pager-col w) dc) (pager-max-col w))])]
+       [(? string?)
+        (gvector-add! (pager-lines w) (text e))
+        (struct-copy pager w
+                     [max-col (max (pager-max-col w) (string-length e))])]
+       [(screen-size-report rows cols)
+        (struct-copy pager w
+                     [rows rows]
+                     [cols cols])]
+       [_
+        w]))
+   (define (word-output w)
+     (define ls (pager-lines w))
+     (define row (add1 (pager-row w)))
+     (define col (add1 (pager-col w)))
+     (crop (sub1 col) (add1 (pager-cols w)) (sub1 row) (add1 (pager-rows w))
+           (place-cursor-after
+            (if (zero? (gvector-count ls))
+              (blank)
+              (vappend* #:halign 'left
+                        (for/list ([e (in-gvector ls)])
+                          e)))
+            (sub1 row) (sub1 col))))])
+
+(define (page src ip)
+  (call-with-chaos
+   (make-raart)
+   (λ ()
+     (fiat-lux (pager src ip 24 80 (make-gvector) 0 0 0)))))
+
+(module+ main
+  (require racket/cmdline)
+  (define to-view
+    (command-line
+     #:program "rage"
+     #:usage-help "racket pager"
+     #:args maybe-file
+     (match maybe-file
+       [(or (list) (list "-")) 'stdin]
+       [(list one) one]
+       [_ (error 'rage "Too many arguments")])))
+  (void
+   (with-input-source to-view page)))
